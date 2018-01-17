d2e984a7ae9060b94ada36b332b081e627fd775a
Steve <sharshman193@gmail.com>
2018-01-17 08:48:59 -0700

    plotting feat correlation

--- eda.py
+++ eda.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+# -*- coding: utf-8 -*-
 
 
 def data_merge():
@@ -8,6 +9,7 @@ def data_merge():
     weather_df = pd.read_csv('data/weather.csv', encoding='ISO-8859-1')
 
     weather_df.columns = [col.lower().replace(' ', '_') for col in weather_df.columns]
+    weather_df.columns = [col.lower().rstrip('_(°f)') for col in weather_df.columns]
     weather_df['date'] = pd.to_datetime(weather_df['date'])
 
     prod_df['date'] = pd.to_datetime(prod_df['VOLUME_DATE'])
@@ -25,13 +27,41 @@ def data_merge():
 
 def plot_prod(df):
     plt.close()
+    fig, ax = plt.subplots(1, 1, figsize=(12, 10))
 
-    plt.plot(df['date'], df['production'])
+    ax.plot(df['date'], df['production'], color='k')
 
+    plt.title('North Gas Production')
+    plt.xlabel('Time')
+    plt.ylabel('Total Daily Gas Production (mcf)')
     plt.savefig('figures/prod.png')
+
+def correlation(df, plt_type='heat'):
+    plt.close()
+    corr = df.corr()
+    fig, ax = plt.subplots(figsize=(10, 10))
+
+    if plt_type.lower() == 'heat':
+        cax = ax.matshow(corr)
+        plt.xticks(range(len(corr.columns)), corr.columns, rotation='vertical')
+        plt.yticks(range(len(corr.columns)), corr.columns)
+        fig.colorbar(cax)
+        plt.title('Correlation Between Production and Weather Data', y=1.35)
+    elif plt_type.lower() == 'scatter':
+        corr_df = df[['date', 'production', 'max_temperature', \
+                      'min_temperature', 'max_dew_point', \
+                      'meandew_point', 'min_dewpoint']]
+        pd.plotting.scatter_matrix(corr_df, alpha=0.3, figsize=(14,8), diagonal='kde')
+        plt.title('Feature Correlation')
+
+    plt.tight_layout()
+
+    plt.savefig('figures/corr_{}.png'.format(plt_type))
 
 
 if __name__ == '__main__':
     df = data_merge()
 
-    plot_prod(df)
+    # plot_prod(df)
+    correlation(df, plt_type='heat')
+    correlation(df, plt_type='scatter')
